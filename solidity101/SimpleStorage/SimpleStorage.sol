0b0ed239328c48e2be002d26fd9cbf5823b99c1a
MichaelChen <helloworldsmart3@gmail.com>
2024-05-25 23:07:21 +0800

    Lesson 3: Basic variable types

--- solidity101/SimpleStorage/SimpleStorage.sol
+++ solidity101/SimpleStorage/SimpleStorage.sol
@@ -3,4 +3,11 @@ pragma solidity 0.8.19; // solidity versions
 
 contract SimpleStorage {
     // Basic Types: boolean, unit, int, address, bytes
+    bool hasFavoriteNumber = false;
+    uint favoriteNumber = 69;
+    uint256 myFavoriteNumber = 969;
+    int256 youFavoriteNumber = -69;
+    string favoriteNumberInText = "69";
+    // address myAddress = 0xaB1B720;
+    bytes32 favorityBytes32 = "cat";
 }
