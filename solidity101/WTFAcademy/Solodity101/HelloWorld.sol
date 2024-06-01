701483d98497e1b2d20b8c101a0a4b369e12bde5
MichaelChen <helloworldsmart3@gmail.com>
2024-06-01 15:54:10 +0800

    value type

--- solidity101/WTFAcademy/Solodity101/HelloWorld.sol
+++ solidity101/WTFAcademy/Solodity101/HelloWorld.sol
@@ -13,4 +13,19 @@ contract HelloWeb3 {
     address public _address = 0x123;
     address payable public _address1 = payable(_address); // _address 轉帳給 _address1
     uint256 public balance = _address1.balance;
+
+    bytes32 public _bytes32 = "MiniSolidity";
+    bytes1 public _byte = _bytes32[0];
+
+    enum ActionSet {
+        Buy,
+        Hold,
+        Sell
+    }
+
+    ActionSet action = ActionSet.Buy;
+
+    function enumToUint() external view returns (uint) {
+        return uint(action);
+    }
 }
