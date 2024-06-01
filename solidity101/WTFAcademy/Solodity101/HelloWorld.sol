800778e4e5786edddaa5049c3b61b25a0678331f
MichaelChen <helloworldsmart3@gmail.com>
2024-06-01 15:12:53 +0800

    wtf.academy 101 :  hello world

---
+++ solidity101/WTFAcademy/Solodity101/HelloWorld.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+contract HelloWeb3 {
+    string public _string = "Hello Web3!";
+    bool public _bool = true;
+
+    uint256 public _number = 20220330;
+    uint256 public _number1 = _number + 1;
+    uint256 public _number2 = 2 ** 2;
+    uint256 public _number3 = 7 % 2;
+    bool public _numberbool = _number2 > _number3;
+}
