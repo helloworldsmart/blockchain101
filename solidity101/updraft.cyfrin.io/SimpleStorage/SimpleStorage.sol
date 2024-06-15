fad4eb10715934ce05f9005afc9fb5354c8b4bd2
MichaelChen <helloworldsmart3@gmail.com>
2024-06-15 16:23:47 +0800

    Lesson 5: Arrays and structs

--- solidity101/updraft.cyfrin.io/SimpleStorage/SimpleStorage.sol
+++ solidity101/updraft.cyfrin.io/SimpleStorage/SimpleStorage.sol
@@ -3,14 +3,25 @@ pragma solidity 0.8.19; // solidity versions
 
 contract SimpleStorage {
     // favoriteNumber gets initialized to 0 if no value
-    uint256 public favoriteNumber; // 0
+    uint256 public myFavoriteNumber; // 0
 
-    function store(uint256 _herFavoriteNumber) public {
-        herFavoriteNumber = _herFavoriteNumber;
+    // uint256[] listOfFavoriteNumbers;
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    // Person public pat = Person(7, "Pat");
+    Person public pat = Person({favoriteNumber: 7, name: "Pat"});
+    Person public mariah = Person({favoriteNumber: 16, name: "Mariah"});
+    Person public jon = Person({favoriteNumber: 12, name: "Jon"});
+
+    function store(uint256 _favoriteNumber) public {
+        myFavoriteNumber = _favoriteNumber;
     }
 
     // view, pure
     function retrieve() public view returns (uint256) {
-        return favoriteNumber;
+        return myFavoriteNumber;
     }
 }
