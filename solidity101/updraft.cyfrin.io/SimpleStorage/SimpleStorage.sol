536415f16af047da89296f024108821138ae6052
MichaelChen <helloworldsmart3@gmail.com>
2024-06-15 15:33:29 +0800

    Lesson 4: Functions

--- solidity101/updraft.cyfrin.io/SimpleStorage/SimpleStorage.sol
+++ solidity101/updraft.cyfrin.io/SimpleStorage/SimpleStorage.sol
@@ -2,19 +2,15 @@
 pragma solidity 0.8.19; // solidity versions
 
 contract SimpleStorage {
-    // Basic Types: boolean, unit, int, address, bytes
-    bool hasFavoriteNumber = false;
-    uint favoriteNumber = 69;
-    uint256 myFavoriteNumber = 969;
-    int256 youFavoriteNumber = -69;
-    string favoriteNumberInText = "69";
-    // address myAddress = 0xaB1B720;
-    bytes32 favorityBytes32 = "cat";
-
     // favoriteNumber gets initialized to 0 if no value
-    uint256 herFavoriteNumber; // 0
+    uint256 public favoriteNumber; // 0
 
     function store(uint256 _herFavoriteNumber) public {
         herFavoriteNumber = _herFavoriteNumber;
+    }
+
+    // view, pure
+    function retrieve() public view returns (uint256) {
+        return favoriteNumber;
     }
 }
